a23c8ad9660aae8c3a78c709d892b2df0b6d6772
lars-danielis <lars@danielis.de>
2015-04-12 20:01:15 +0200

    diverse kleinere Anpassungen

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -617,9 +617,9 @@ const bool Z_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the logic
 // @section temperature
 
 // Preheat Constants
-#define PLA_PREHEAT_HOTEND_TEMP 180
+#define PLA_PREHEAT_HOTEND_TEMP 200
 #define PLA_PREHEAT_HPB_TEMP 70
-#define PLA_PREHEAT_FAN_SPEED 255   // Insert Value between 0 and 255
+#define PLA_PREHEAT_FAN_SPEED 0   // Insert Value between 0 and 255
 
 #define ABS_PREHEAT_HOTEND_TEMP 240
 #define ABS_PREHEAT_HPB_TEMP 120
